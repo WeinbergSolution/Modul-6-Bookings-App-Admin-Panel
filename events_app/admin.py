b42c0dafb4cfe6a55f71da2421cfb022a8e52c68
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-22 10:22:00 +0200

    feat: complete Django admin panel exercises 1-4

--- events_app/admin.py
+++ events_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import EventCategory, Location, Event
+
+
+
+
+
+class EventAdmin(admin.ModelAdmin):
+
+    list_filter=['category']
+    search_fields=['title', 'date']
+    list_display=['title', 'category', 'location', 'date']
+   
+
+   
+
+
 
 # Register your models here.
+# Durch die Registrierung werden die Models im Django Admin Panel
+# sichtbar und können dort verwaltet werden.
+
+admin.site.register(EventCategory)
+admin.site.register(Location)
+admin.site.register(Event, EventAdmin)
